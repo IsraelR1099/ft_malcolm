9afdfa4be65fb62f5675bca40a7bab2113ae13bb
Israel Rifarachi hoyos <irifarac@cbr12s5.42barcelona.com>
2025-01-27 13:38:31 +0100

    starting ft_malcolm

---
+++ Libft/src/ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 17:45:37 by irifarac          #+#    #+#             */
+/*   Updated: 2025/01/27 12:46:53 by irifarac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	lenstr;
+	size_t	counter;
+
+	if (!s)
+		return (NULL);
+	lenstr = ft_strlen(s);
+	if (len >= (lenstr - start))
+		len = lenstr - start;
+	if (start > lenstr)
+		len = 0;
+	ptr = (char *)malloc(sizeof(*ptr) * (len + 1));
+	if (!ptr)
+		return (0);
+	counter = 0;
+	while (counter < len && start < ft_strlen(s))
+		ptr[counter++] = s[start++];
+	ptr[counter] = '\0';
+	return (ptr);
+}
