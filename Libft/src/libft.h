64477d52e0c387ef60aa5768f379e6d28d9dd19e
IsraelR1099 <rifarachi10@gmail.com>
2025-02-13 23:11:32 +0100

    adding strncpy to my libft

--- Libft/src/libft.h
+++ Libft/src/libft.h
@@ -6,7 +6,7 @@
 /*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:30:04 by irifarac          #+#    #+#             */
-/*   Updated: 2022/02/23 12:41:55 by irifarac         ###   ########.fr       */
+/*   Updated: 2025/02/13 23:01:19 by israel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ void	*ft_memset(void *dest, int ch, size_t count);
 char	*ft_strchr(const char *str, int ch);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strncpy(char *dst, const char *src, size_t n);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *str1, const char *str2, size_t number);
 char	*ft_strnstr(const char *dest, const char *src, size_t count);
